1136871c922c9a4d1b6bceb6f3981bf8c0a1f741
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-12 23:02:25 -0500

    Question analysis and algorithm

--- src/com/codewithdelayne/DiagonalDifference.java
+++ src/com/codewithdelayne/DiagonalDifference.java
@@ -21,3 +21,47 @@ public class DiagonalDifference {
     }
 
 }
+
+
+//  Question Analysis
+
+// given: sq matrix = 2D
+// calculate: absolute difference between sums and diagonals
+// function description: diagonalDifference takes the following parameter: int arr[n][m]
+// return: int absolute diagonal difference
+
+
+
+//  Algorithm
+
+// row arr[i]
+//column arr[i][j]
+
+// Primary PATTERN
+
+// traverse++ = [i][j]
+// add to primary counter
+// traverse++ = [i+1][j+1]
+// add to primary counter
+// traverse++ = [i+2][j+2]
+// add to primary counter
+// calculate the sum across the primary diagonal
+
+
+
+// Secondary PATTERN
+
+// reverse traverse
+// traverse-- = [i][j]
+// add to primary counter
+// traverse-- = [i+1][j-1]
+// add to primary counter
+// traverse-- = [i+2][j-2]
+// add to primary counter
+// calculate the sum across the secondary diagonal
+
+
+// n= # of rows and columns
+
+// calculate the absolute difference Math.abs()
+//clear counters
