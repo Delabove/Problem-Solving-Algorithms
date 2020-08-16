35585e1d99d48bc40177aaeb5896471851c667a2
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-16 14:40:26 -0500

    Question analysis, basic structure, and testing of sorting before writing function

---
+++ src/com/codewithdelayne/BirthdayCakeCandles.java
@@ -0,0 +1,59 @@
+package com.codewithdelayne;
+
+import java.util.Arrays;
+
+public class BirthdayCakeCandles {
+    static int[] birthdayCakeCandles(int[] ar) {
+    int numOfTallest = 0;
+
+    Arrays.sort(ar);
+//      search array for tallest (max)
+//      count how many tallest (max)
+//      print sum of tallest (max)
+
+    return ar;
+    }
+
+    public static void main(String[] args) {
+        int[] ar = new int[]{2,3,1,3};
+        System.out.println(Arrays.toString(birthdayCakeCandles(ar)));
+    }
+
+}
+
+
+//      Question Analysis
+
+// 1 candle (for each) year of her total age
+// can only blow out tallest candles
+// task: how many candles can successfully blow out
+
+// ex: if 4yrs old, 4 candles of height 4,4,1,3
+// will be able to blow out 2 candles
+// since height of tallest = 4 and there are 2
+
+//function description:return integer representing # of candles she can blow out
+//parameters: array of integers rep candle heights
+
+// input: 1st line: n = # of candles on cake
+//        2nd line: n space separated integers, each i = height of candle i
+
+//constraint:
+// 1 <= n <= 100000
+// 1 <= arr[i] <= 10000000
+
+// output: # of candles can be blown out
+
+// sample input     sample output
+// 4
+//3 2 1 3                   2
+
+//explanation:
+
+// we have [1, 2, 3, 3]
+// only blows out: 3
+//print: 2 (two of tallest candles)
+
+//search array for tallest (max)
+//count how many tallest (max)
+//print sum of tallest (max)
--- src/com/codewithdelayne/MiniMaxSum.java
+++ src/com/codewithdelayne/MiniMaxSum.java
@@ -5,8 +5,8 @@ public class MiniMaxSum {
     static void miniMaxSum(int[] arr) {
 
         Arrays.sort(arr);
-        int min = 0;
-        int max = 0;
+        long min = 0;
+        long max = 0;
 
         for(int i = 0; i < arr.length - 1; i++){
             min = min + arr[i];
