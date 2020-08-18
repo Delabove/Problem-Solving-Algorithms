ad220a52ba8e510487936c0eff1345ae7642ef4d
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-18 00:28:13 -0500

    Binary tree challenge, cannot print root, only tree

--- src/com/codewithdelayne/BinaryTreef1.java
+++ src/com/codewithdelayne/BinaryTreef1.java
@@ -1,10 +1,5 @@
 package com.codewithdelayne;
 
-import org.w3c.dom.Node;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class BinaryTreef1 {
 
 
@@ -50,26 +45,15 @@ public class BinaryTreef1 {
             return node;
         }
 
-        /* A utility function to print preorder traversal of BST */
-        void preOrder(Node node) {
-            if (node == null) {
-                return;
-            }
-            System.out.print(node.data + " ");
-            preOrder(node.left);
-            preOrder(node.right);
-        }
-
-
     }
+
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
-        int arr[] = new int[]{1, 2, 3, 4, 5, 6, 7};
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7};
         int n = arr.length;
         Node root = tree.balancedTree(arr, 0, n - 1);
-        System.out.println("Preorder traversal of constructed BST");
-        tree.preOrder(root);
-        System.out.println(root);
+
+
     }
 
 }
@@ -82,25 +66,3 @@ public class BinaryTreef1 {
 // create a balanced binary tree
 
 // return the root of the tree as your result.
-
-//
-//ArrayList<Integer> inputList;
-//    Node node = balancedTree(inputList);
-//
-//    private Node balancedTree(ArrayList<Integer> inputList2){
-//        return balancedTree(inputList, 0, inputList.size() - 1);
-//    }
-//    public static Node balancedTree(ArrayList<Integer> inputList, int start, int end){
-//
-//
-//
-//        if(end<start)
-//            return null;
-//
-//        int mid = (start+end)/2;
-//        Node node= new Node(inputList.get(mid));
-//        node.left = balancedTree(inputList, start, mid -1);
-//        node.right = balancedTree(inputList, mid + 1, end);
-//
-//        return node;
-//    }
