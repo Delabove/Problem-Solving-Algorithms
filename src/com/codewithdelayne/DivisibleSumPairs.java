83d9a976a5484b5c45de3306be800b00c459b59a
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-18 18:41:40 -0500

    Algorithm correct, result incorrect

---
+++ src/com/codewithdelayne/DivisibleSumPairs.java
@@ -0,0 +1,40 @@
+package com.codewithdelayne;
+
+public class DivisibleSumPairs {
+
+    // Complete the divisibleSumPairs function below.
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+         int pairs = 0;
+        for(int i = 0; i < ar.length; i++){
+            for(int j = 0; j < ar.length; j++){
+                if(i < j && (ar[i] + ar[j]) % k == 0){
+                    pairs++;
+
+                }
+            }
+
+        }
+        return pairs;
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] ar = new  int[]{1, 3, 2, 6, 1, 2};
+    }
+
+}
+//          Question Summary
+
+// Given: array of n integers, ar= [ar[0], ar[1], ... ar[n-1]
+// and a positive integer, k
+// find and print:
+//
+// (i, j) pairs where
+// i < j &&
+// ar[i] + ar[j] is divisible by k
+
+
+//                  input:
+//  (# of items)    n=6 k=3 (divisor)
+// values of array: 1 3 2 6 1 2
