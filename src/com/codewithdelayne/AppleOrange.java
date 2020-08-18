a36a16285f57c61b87dbca30ac538cbc4d0762ff
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-18 12:00:48 -0500

    Algorithm correct, incorrect output

--- src/com/codewithdelayne/AppleOrange.java
+++ src/com/codewithdelayne/AppleOrange.java
@@ -1,15 +1,58 @@
 package com.codewithdelayne;
 
+import java.util.Arrays;
+
 public class AppleOrange {
 
+//      Algorithm
+// for loop:
+//  The first apple falls at position 5-2=3.
+//  The second apple falls at position 5+2=7 .
+//  The third apple falls at position 5+1=6.
+//  for loop
+//  The first orange falls at position 15+5=20.
+//  The second orange falls at position 15-6=9.
+//  for each apple of apples between 7 & 11, count
+//  Only one fruit (the second apple) falls within the region between 7 and 11, so we print 1 as our first line of output.
+// //  for each orange of oranges between 7 & 11, count
+// Only the second orange falls within the region between 7 and 11, so we print 1 as our second line of output.
+// new int[] applesAndOranges = [a,o]
+// return applesAndOranges
 
-    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+    static int[] countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        int appleCount = 0;
+        int orangeCount=0;
+        int[] applesAndOranges = new int[2];
 
+        for(int i = 0; i<apples.length; i++){
+            int temp = i + a;
+            if(temp >= s && temp <= t){
+                appleCount++;
+            }
+            applesAndOranges[0]=appleCount;
+        }
+        for(int j = 0; j<oranges.length; j++){
+            int temp = j+b;
+            if(temp >= s && temp <= t){
+               orangeCount++;
+            }
+            applesAndOranges[1] = orangeCount;
+        }
 
+        System.out.println(appleCount);
+        System.out.println(orangeCount);
+        return applesAndOranges;
     }
 
     public static void main(String[] args) {
+//        int s = 7;
+//        int t = 11;
+//        int a = 5;
+//        int b = 15;
+        int[] apples = new int[]{-2,2,1}; //(3)
+        int[] oranges = new int[]{5,-6}; //2
 
+        System.out.println(Arrays.toString(countApplesAndOranges(7, 11, 5, 15, apples, oranges)));
     }
 }
 //      QUESTION ANALYSIS
@@ -47,10 +90,21 @@ public class AppleOrange {
 // m= 3 n=2
 // a= -2 2 1
 // b= 5 -6
-//
-//
-//
-//
+
+//      Algorithm
+// for loop:
+//  The first apple falls at position 5-2=3.
+//  The second apple falls at position 5+2=7 .
+//  The third apple falls at position 5+1=6.
+//  for loop
+//  The first orange falls at position 15+5=20.
+//  The second orange falls at position 15-6=9.
+//  for each apple of apples between 7 & 11, count
+//  Only one fruit (the second apple) falls within the region between 7 and 11, so we print 1 as our first line of output.
+// //  for each orange of oranges between 7 & 11, count
+// Only the second orange falls within the region between 7 and 11, so we print 1 as our second line of output.
+// new int[] applesAndOranges = [a,o]
+// return applesAndOranges
 //
 //
 //
