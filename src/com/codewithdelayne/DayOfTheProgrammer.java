0ecdf5634b581042d5a079da0e09d1d13cf96f08
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-18 22:49:59 -0500

    Did not add the alternative cases to the algorithm making algorithm fail

--- src/com/codewithdelayne/DayOfTheProgrammer.java
+++ src/com/codewithdelayne/DayOfTheProgrammer.java
@@ -3,12 +3,18 @@ package com.codewithdelayne;
 public class DayOfTheProgrammer {
     static String dayOfProgrammer(int year) {
 
-         if (year % 4 == 0  ) {
-             return ("12.09." + year);
-         } else
-                return "13.09." + year;
+        String date = "";
+        if(year < 1918) {                                                   //Julian check for leap year
+            date += (year % 4 == 0) ? "12.09." + year : "13.09." + year;
+        } else if(year == 1918) {                                           //Special case: transition year
+            date += "26.09." + year;
+        } else {                                                            //Gregorian check for leap year
+            date += ((year % 400 == 0) ||
+                    (year % 4 == 0 && year % 100 != 0)) ? "12.09." + year : "13.09." + year;
+        }
+        return date;
+    }
 
-            }
 
 
     public static void main(String[] args) {
