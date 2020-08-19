86c958702ec8fe1b8cdb2d1a12d4af6852fdadc3
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-19 12:57:31 -0500

    question analysis and algorithm

---
+++ src/com/codewithdelayne/CatsMouse.java
@@ -0,0 +1,42 @@
+package com.codewithdelayne;
+
+public class CatsMouse {
+
+    static String catAndMouse(int x, int y, int z) {
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(catAndMouse(1,3,2));
+    }
+}
+
+//      Question Analysis
+
+//
+// given: their starting positions
+//
+// task: determine which cat will reach the mouse first, assuming the mouse doesn't move and the cats travel at equal speed.
+// If the cats arrive at the same time, the mouse will be allowed to move and it will escape while they fight.
+//
+// You are given q queries in the form of x, y, and z representing the respective positions for cats A and B, and for mouse C.
+//
+//  If cat A catches the mouse first, print Cat A.
+//  If cat B catches the mouse first, print Cat B.
+//  If both cats reach the mouse at the same time, print Mouse C as the two cats fight and mouse escapes.
+
+//  Sample Input 0
+//
+//  2 ->number of queries
+//  1 2 3  (cat A's location),  (cat B's location), and  (mouse C's location).
+//  1 3 2
+
+// create array to index cat's and mouse position
+// loop through to find given location
+// if distance between A & C is < B
+// print Cat A
+// if distance between A & C is > B
+// print Cat B
+// if distance of A from C is == distance of B from C
+// print Mouse C
