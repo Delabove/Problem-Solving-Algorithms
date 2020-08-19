041cfa87a9070a9c956587743f9310c8d397a15f
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-18 19:06:44 -0500

    Question analysis and algorithm

---
+++ src/com/codewithdelayne/MigratoryBirds.java
@@ -0,0 +1,49 @@
+package com.codewithdelayne;
+
+import java.util.List;
+
+public class MigratoryBirds {
+
+
+
+    static int migratoryBirds(List<Integer> arr) {
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+//              Question Analysis
+
+// arrayList = sightings
+// number = type of bird
+//
+
+//
+// func descr: return the lowest type number of the most frequently sighted bird.
+//      param: arr -int representing types of birds sighted
+//
+// input
+// # of birds sighted and reported in arr                 6
+// type # of each bird sighted                            1 4 4 4 5 3
+// output
+// Print the type number of the most common bird;
+// if two or more types of birds are equally common,        4
+// choose the type with the smallest ID number.
+//
+//
+// loop through arr
+// if arr[i] appears more than once
+// store number in a temporary integer
+// add 1 to counter for every occurrence
+// if another arr[i] occurs more than once
+//  store in temporary integer
+//  count
+// if count > previous occurence
+// add new number to most common integer
+// if more than 1 int == the same occurence, return the lowest of the two numbers
+//
