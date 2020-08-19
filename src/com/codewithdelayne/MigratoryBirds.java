681d8fa4f0f1248314dfb8fb7d57bf2c2a3ac3b6
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-18 20:13:33 -0500

    Algorithm not quite working, adjustments needed to counter, returning highest integer, not integer with the highest occurences

--- src/com/codewithdelayne/MigratoryBirds.java
+++ src/com/codewithdelayne/MigratoryBirds.java
@@ -1,17 +1,54 @@
 package com.codewithdelayne;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MigratoryBirds {
 
 
+// loop through arr
+// if arr[i] appears more than once
+// store number in a temporary integer
+// add 1 to counter for every occurrence
+// if another arr[i] occurs more than once
+//  store in temporary integer
+//  count
+// if count > previous occurence
+// add new number to most common integer
+// if more than 1 int == the same occurence, return the lowest of the two numbers
+//
+
+
 
     static int migratoryBirds(List<Integer> arr) {
+        int count = 0;
+        int tempCounter = 0;
+        int mostCommon = 0;
+    for(int i = 0; i < arr.size(); i++){
+        while(arr.get(i) > 1){
+          int temp = arr.get(i);
+          tempCounter++;
 
+             if(tempCounter > count){
+                mostCommon = arr.get(i);
+                 count = tempCounter;
+             }
+        }
+
+    } return mostCommon;
 
     }
 
     public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(4);
+        arr.add(4);
+        arr.add(4);
+        arr.add(5);
+        arr.add(3);
+
+        System.out.println(migratoryBirds(arr));
 
     }
 
